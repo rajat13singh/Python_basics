1027647b9b6a06499bebc59a23f8e4a09c22992e
rajat13singh <rajatsingh18117@gmail.com>
2026-04-25 17:27:13 +0530

    Delete first_git-main directory

--- first_git-main/CONDITIONAL STATEMENTS/simpleif.py
+++
@@ -1,8 +0,0 @@
-#for checking one condition we use simple if
-colour=input("enter the colour:")
-if colour=="red":
-    print("stop")
-elif colour=="yellow":
-    print("ready")
-elif colour=="green":
-    print("go")
